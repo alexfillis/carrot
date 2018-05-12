3383aaf1c9b3826c52e1f94796e7aab59f5c8963
Alex Fillis <alex.fillis@gmail.com>
2018-05-12 13:49:54 +0800

    #2 Extract handler functions to improve readability by reducing the size of the blocks of code.

--- index.js
+++ index.js
@@ -17,50 +17,56 @@ var imap = new Imap({
     tls: config.imap.host.secure
 });
 
-function openInbox(cb) {
-    imap.openBox('INBOX', true, cb);
-}
-
-imap.once('ready', function() {
-    openInbox(function(err, box) {
-        if (err) throw err;
-        var f = imap.seq.fetch(box.messages.total + ':*', { bodies: ['HEADER.FIELDS (FROM)','TEXT'] });
-        f.on('message', function(msg, seqno) {
-            console.log('Message #%d', seqno);
-            var prefix = '(#' + seqno + ') ';
-            msg.on('body', function(stream, info) {
-                if (info.which === 'TEXT')
-                    console.log(prefix + 'Body [%s] found, %d total bytes', inspect(info.which), info.size);
-                var buffer = '', count = 0;
-                stream.on('data', function(chunk) {
-                    count += chunk.length;
-                    buffer += chunk.toString('utf8');
-                    if (info.which === 'TEXT')
-                        console.log(prefix + 'Body [%s] (%d/%d)', inspect(info.which), count, info.size);
-                });
-                stream.once('end', function() {
-                    if (info.which !== 'TEXT')
-                        console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
-                    else
-                        console.log(prefix + 'Body [%s] Finished', inspect(info.which));
-                });
-            });
-            msg.once('attributes', function(attrs) {
-                console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
-            });
-            msg.once('end', function() {
-                console.log(prefix + 'Finished');
-            });
+const messageHandler = (msg, seqno) => {
+    console.log('Message #%d', seqno);
+    var prefix = '(#' + seqno + ') ';
+    msg.on('body', function (stream, info) {
+        if (info.which === 'TEXT')
+            console.log(prefix + 'Body [%s] found, %d total bytes', inspect(info.which), info.size);
+        var buffer = '', count = 0;
+        stream.on('data', function (chunk) {
+            count += chunk.length;
+            buffer += chunk.toString('utf8');
+            if (info.which === 'TEXT')
+                console.log(prefix + 'Body [%s] (%d/%d)', inspect(info.which), count, info.size);
         });
-        f.once('error', function(err) {
-            console.log('Fetch error: ' + err);
-        });
-        f.once('end', function() {
-            console.log('Done fetching all messages!');
-            imap.end();
+        stream.once('end', function () {
+            if (info.which !== 'TEXT')
+                console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
+            else
+                console.log(prefix + 'Body [%s] Finished', inspect(info.which));
         });
     });
-});
+    msg.once('attributes', function (attrs) {
+        console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
+    });
+    msg.once('end', function () {
+        console.log(prefix + 'Finished');
+    });
+};
+
+const errorHandler = err => {
+    console.log('Fetch error: ' + err);
+};
+
+const endHandler = () => {
+    console.log('Done fetching all messages!');
+    imap.end();
+};
+
+const openHandler = (err, box) => {
+    if (err) throw err;
+    var f = imap.seq.fetch(box.messages.total + ':*', {bodies: ['HEADER.FIELDS (FROM)', 'TEXT']});
+    f.on('message', messageHandler);
+    f.once('error', errorHandler);
+    f.once('end', endHandler);
+};
+
+const readyHandler = () => {
+    imap.openBox('INBOX', true, openHandler);
+};
+
+imap.once('ready', readyHandler);
 
 imap.once('error', function(err) {
     console.log(err);
