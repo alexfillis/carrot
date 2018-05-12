a4c6015ffe266d036aa7d6e15bc630b4d3efe1c5
Alex Fillis <alex.fillis@gmail.com>
2018-05-12 13:55:21 +0800

    #1 Some more example code for: Save raw unread emails since May 20, 2010 to files.

--- index.js
+++ index.js
@@ -22,42 +22,34 @@ function openInbox(cb) {
 }
 
 imap.once('ready', function() {
+    var fs = require('fs'), fileStream;
+
     openInbox(function(err, box) {
         if (err) throw err;
-        var f = imap.seq.fetch(box.messages.total + ':*', { bodies: ['HEADER.FIELDS (FROM)','TEXT'] });
-        f.on('message', function(msg, seqno) {
-            console.log('Message #%d', seqno);
-            var prefix = '(#' + seqno + ') ';
-            msg.on('body', function(stream, info) {
-                if (info.which === 'TEXT')
-                    console.log(prefix + 'Body [%s] found, %d total bytes', inspect(info.which), info.size);
-                var buffer = '', count = 0;
-                stream.on('data', function(chunk) {
-                    count += chunk.length;
-                    buffer += chunk.toString('utf8');
-                    if (info.which === 'TEXT')
-                        console.log(prefix + 'Body [%s] (%d/%d)', inspect(info.which), count, info.size);
+        imap.search([ 'UNSEEN', ['SINCE', 'May 20, 2010'] ], function(err, results) {
+            if (err) throw err;
+            var f = imap.fetch(results, { bodies: '' });
+            f.on('message', function(msg, seqno) {
+                console.log('Message #%d', seqno);
+                var prefix = '(#' + seqno + ') ';
+                msg.on('body', function(stream, info) {
+                    console.log(prefix + 'Body');
+                    stream.pipe(fs.createWriteStream('msg-' + seqno + '-body.txt'));
                 });
-                stream.once('end', function() {
-                    if (info.which !== 'TEXT')
-                        console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
-                    else
-                        console.log(prefix + 'Body [%s] Finished', inspect(info.which));
+                msg.once('attributes', function(attrs) {
+                    console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
+                });
+                msg.once('end', function() {
+                    console.log(prefix + 'Finished');
                 });
             });
-            msg.once('attributes', function(attrs) {
-                console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
+            f.once('error', function(err) {
+                console.log('Fetch error: ' + err);
             });
-            msg.once('end', function() {
-                console.log(prefix + 'Finished');
+            f.once('end', function() {
+                console.log('Done fetching all messages!');
+                imap.end();
             });
-        });
-        f.once('error', function(err) {
-            console.log('Fetch error: ' + err);
-        });
-        f.once('end', function() {
-            console.log('Done fetching all messages!');
-            imap.end();
         });
     });
 });
