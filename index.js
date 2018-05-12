d33b71e1dcb58c7b99c630436b0cc8453247788d
Alex Fillis <alex.fillis@gmail.com>
2018-05-12 15:07:14 +0800

    #2 Integrate example code for persisting the mails to the filesystem.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const config = require('./config');
 const Imap = require('imap'),
     inspect = require('util').inspect;
+var fs = require('fs'), fileStream;
 
 const app = express();
 
@@ -56,10 +57,31 @@ const endHandler = () => {
 
 const openHandler = (err, box) => {
     if (err) throw err;
-    const f = imap.seq.fetch(box.messages.total + ':*', {bodies: ['HEADER.FIELDS (FROM)', 'TEXT']});
-    f.on('message', messageHandler);
-    f.once('error', errorHandler);
-    f.once('end', endHandler);
+    imap.search([ 'UNSEEN', ['SINCE', 'May 20, 2010'] ], function(err, results) {
+        if (err) throw err;
+        var f = imap.fetch(results, { bodies: '' });
+        f.on('message', function(msg, seqno) {
+            console.log('Message #%d', seqno);
+            var prefix = '(#' + seqno + ') ';
+            msg.on('body', function(stream, info) {
+                console.log(prefix + 'Body');
+                stream.pipe(fs.createWriteStream('msg-' + seqno + '-body.txt'));
+            });
+            msg.once('attributes', function(attrs) {
+                console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
+            });
+            msg.once('end', function() {
+                console.log(prefix + 'Finished');
+            });
+        });
+        f.once('error', function(err) {
+            console.log('Fetch error: ' + err);
+        });
+        f.once('end', function() {
+            console.log('Done fetching all messages!');
+            imap.end();
+        });
+    });
 };
 
 const readyHandler = () => {
