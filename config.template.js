54c0e290aa549650a687fda8e8e4d4b923327260
Alex Fillis <alex.fillis@gmail.com>
2018-05-13 21:35:21 +0800

    #5 Add a template for the configuration detail.

---
+++ config.template.js
@@ -0,0 +1,14 @@
+// copy and rename to 'config.js'; provide the details for your environment
+module.exports = {
+    imap: {
+        host: {
+            name: "host.name.com",
+            port: "993",
+            secure: true
+        },
+        user: {
+            name: "market.carrot.test@example.com",
+            password: "CarrotPassword"
+        }
+    }
+};
